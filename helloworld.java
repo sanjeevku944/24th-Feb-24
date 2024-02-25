0fc0b6afae58278c9c8b9159f60fd43a4a252fb6
sanjeevku944 <150313710+sanjeevku944@users.noreply.github.com>
2024-02-25 23:55:43 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Sanjeev Kumar"); 
+        System.out.println("Hello, World! Sanjeev Kumar,Pappu Kumar"); 
     }
 }
